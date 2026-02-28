84e5af28911ca433e00c7bdb301b06710b609921
Pham Hoang Vu <phamvuhoang486@gmail.com>
2026-02-28 13:39:32 +0700

    update: install library and create file

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("ok");
+import express, { Express, Request, Response} from "express";
+
+const app: Express = express();
+const port: number = 3000;
+
+app.set("views", "./view");
+app.set("view engine", "pug");
+
+app.get("/topics", (req: Request, res: Response) => {
+    res.render("client/pages/topics/index");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
